e350e79530705cb1b003550766b86d6f640db7cf
khusaini <@muhammadkhusaini599>
2022-03-11 16:19:15 +0700

    Literal

---
+++ src/main/kotlin/2_Operator.kt
@@ -0,0 +1,20 @@
+fun main() {
+    //komentar 1 baris
+    /*
+    1 baris atau lebih
+     */
+    /*
+    *dokumentasi
+     */
+    var a = 7
+    var b = 6
+    var c =(a++ + b++) /2
+    println(c+b)
+    var dinamis="hore"
+    var statis=2.0
+    println(statis)
+    statis=3.0
+    statis=5.9
+    val phi=3.14
+
+}
