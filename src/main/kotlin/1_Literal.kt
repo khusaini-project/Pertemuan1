0f35d10342d0f7ce42b5d841d7c5a3f38456784e
khusaini <@muhammadkhusaini599>
2022-03-11 15:07:46 +0700

    Initial project pertemuan1

--- src/main/kotlin/1_Literal.kt
+++ src/main/kotlin/1_Literal.kt
@@ -1,11 +1,11 @@
 fun main() {
-    //Deklarasi dan inisialisasi variabel-variabel,..
+    //Deklarasi dan inisialisasi variabel-variabel,...
     var intLiteral = 5
     var doubleLiteral = .02
     var stringLiteral = "Hello"
     var charLiteral = '1'
     var boolLiteral = true
-    //Tampilkan variabel menggunakan string template,..
+    //Tampilkan variabel menggunakan string template,...
     println("intLiteral = $intLiteral")
     println("doubleLiteral = $doubleLiteral")
     println("stringLiteral = $stringLiteral")
