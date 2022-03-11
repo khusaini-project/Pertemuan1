5ab247cbfbd6fd6cfc4f9f85da456f867f2ec75d
khusaini <@muhammadkhusaini599>
2022-03-11 16:56:45 +0700

    Character

---
+++ src/main/kotlin/6_Character.kt
@@ -0,0 +1,12 @@
+fun main() {
+    // penggunaan fungsi anggota dari Character seperti isLowerCase(),isDigit(),toUpperCase().
+
+    var charA : Char = 'A'
+    //println(charA == 65)
+    println(charA.isUpperCase())
+    println(charA.isLowerCase())
+    println(charA.isDigit())
+    println(charA.toLowerCase())
+    val strA: String = charA.toString()
+    println("Kini charA sudah menjadi String = "+strA)
+}
