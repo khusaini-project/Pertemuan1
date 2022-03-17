8fc2fea67c5b4b96ec965dc5e0de43926044baf4
khusaini <@muhammadkhusaini599>
2022-03-17 22:30:35 +0700

    Penerapan Konversi Eksplisit pada Char

---
+++ src/main/kotlin/PenerapanKonversiEksplisitPadaChar.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    var c: Char // character
+    var i: Int // ordinal (ASCII) value of the character
+
+    c = 'a'
+    i = c.toInt()
+    println("The character $c was converted to its ASCII value of $i")
+
+    i = 98
+    c = i.toChar()
+    println("The ASCII value of $i was converted to its textual value of $c")
+}
